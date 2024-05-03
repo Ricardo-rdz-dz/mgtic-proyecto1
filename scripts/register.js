5e76e23b9c1d8b649400545db3439ea9c1cdfc47
Ricardo-rdz-dz <drums417@gmail.com>
2024-05-02 23:43:13 -0700

    se agrego mas diseno y facultades

--- scripts/register.js
+++ scripts/register.js
@@ -2,9 +2,16 @@ let estudiantes=[];
 
 // constructor
 
-function Estudiante(p1, p2){
+function Estudiante(p1, p2, p3, p4, p5, p6, p7, p8, p9){
     this.nombre=p1;
     this.edad=p2;
+    this.genero=p3;
+    this.correo=p4;
+    this.password=p5;
+    this.materia1=p6;
+    this.materia2=p7;
+    this.materia3=p8;
+    this.facultad=p9;
 }
 
 
@@ -32,7 +39,15 @@ function esValido(unAlumno){
 function register(){
     let inputNombre = document.getElementById("txtNombre").value;
     let inputEdad = document.getElementById("txtEdad").value;
-    let nuevoAlumno = new Estudiante(inputNombre,inputEdad);
+    let inputGenero = document.getElementById("txtGenero").value;
+    let inputCorreo = document.getElementById("txtCorreo").value;
+    let inputPassword = document.getElementById("txtPassword").value;
+    let inputMateria1 = document.getElementById("txtMateria1").value;
+    let inputMateria2 = document.getElementById("txtMateria2").value;
+    let inputMateria3 = document.getElementById("txtMateria3").value;
+    let inputFacultad = document.getElementById("txtFacultad").value;
+    
+    let nuevoAlumno = new Estudiante(inputNombre, inputEdad, inputCorreo, inputGenero, inputPassword, inputMateria1, inputMateria2, inputMateria3, inputFacultad);
     if(esValido(nuevoAlumno)){
         estudiantes.push(nuevoAlumno);
     console.log(estudiantes);
